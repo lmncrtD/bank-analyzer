e4650813c7a6e23dd8ce9d2638c647bdbaf6fa93
Lmncrt <thegord@yandex.ru>
2026-04-08 23:15:30 +0300

    Add bank analyzer functions

---
+++ src/bank_analyzer/__main__.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from src.bank_analyzer import get_top_transaction
+from src.bank_analyzer import get_total_by_type
+from src.bank_analyzer import get_transactions_by_account
+from src.bank_analyzer import generate_report
+from src.bank_analyzer import load_transactions
+
+def main() -> None:
+    data_path = Path(__file__).parent.parent.parent
+    loader = load_transactions(data_path / "data" / "sample" / "transactions.csv")
+    print(get_total_by_type(loader, 'deposit'))
+    print(get_transactions_by_account(loader, 'ACC001'))
+    print(get_top_transaction(loader))
+    print(data_path / "data" / "sample")
+    generate_report(loader, data_path / "data" / "sample" / "report.json")
+
+if __name__ == "__main__":
+    main()
---
+++ src/bank_analyzer/loader.py
@@ -0,0 +1,17 @@
+import csv
+from pathlib import Path
+
+def load_transactions(file_path: Path) -> list[dict]:
+    result: list[dict] = []
+
+    if not file_path.exists():
+        raise FileNotFoundError(f'Файл не найден: {file_path}')
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            result.append(row)
+
+    return result
+
