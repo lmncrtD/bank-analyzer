1e979801bc4c902cff569acd75dcc477351a2aa2
Lmncrt <thegord@yandex.ru>
2026-04-10 16:17:24 +0300

    Change import's

--- src/bank_analyzer/__init__.py
+++ src/bank_analyzer/__init__.py
@@ -1,8 +1,8 @@
-from src.bank_analyzer.loader import load_transactions
-from src.bank_analyzer.processor import get_total_by_type
-from src.bank_analyzer.processor import get_transactions_by_account
-from src.bank_analyzer.processor import get_top_transaction
-from src.bank_analyzer.report import generate_report
+from bank_analyzer.loader import load_transactions
+from bank_analyzer.processor import get_total_by_type
+from bank_analyzer.processor import get_transactions_by_account
+from bank_analyzer.processor import get_top_transaction
+from bank_analyzer.report import generate_report
 
 """Bank transaction analyzer package."""
 __version__ = "0.0.1"
