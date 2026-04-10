2088de2c240254ab656a7a83bfb917365cd5c8b1
Lmncrt <thegord@yandex.ru>
2026-04-10 16:45:40 +0300

    Change logic for test_processor

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -5,25 +5,15 @@ from bank_analyzer import get_transactions_by_account
 from bank_analyzer import get_top_transaction
 
 @pytest.fixture
-def sample_csv(tmp_path):
-    result: list[dict] = []
-    csv_file = tmp_path / "sample.csv"
-    csv_file.write_text(
-        "account_id,amount,type\n"
-        "ACC001,2000.00,deposit\n"
-        "ACC003,3000.00,withdrawal\n"
-        "ACC001,4000.00,deposit\n"
-        "ACC004,100000.00,withdrawal\n"
-        "ACC001,100000.00,deposit\n",
-        encoding="utf-8",
-    )
-    with open(csv_file, "r", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            row['amount'] = float(row['amount'])
-            result.append(row)
+def sample_csv():
+    return [
+        {"account_id": "ACC001", "amount": 2000.00, "type": "deposit", "date": "2021-04-01"},
+        {"account_id": "ACC003", "amount": 3000.00, "type": "withdrawal", "date": "2021-04-01"},
+        {"account_id": "ACC001", "amount": 4000.00, "type": "deposit", "date": "2021-04-01"},
+        {"account_id": "ACC004", "amount": 100000.00, "type": "withdrawal", "date": "2021-04-01"},
+        {"account_id": "ACC001", "amount": 100000.00, "type": "deposit", "date": "2021-04-01"}
+    ]
 
-    return result
 
 def test_correct_total_by_type(sample_csv):
     assert get_total_by_type(sample_csv, "deposit") >= 0.0
@@ -36,15 +26,15 @@ def test_correct_total_by_type(sample_csv):
 def test_correct_transactions_by_account(sample_csv):
     assert "ACC001" in [x['account_id'] for x in get_transactions_by_account(sample_csv, "ACC001")]
     assert get_transactions_by_account(sample_csv, "ACC001") == [
-        {"account_id": "ACC001", "amount": 2000.00, "type": "deposit"},
-        {"account_id": "ACC001", "amount": 4000.00, "type": "deposit"},
-        {"account_id": "ACC001", "amount": 100000.00, "type": "deposit"}
+        {"account_id": "ACC001", "amount": 2000.00, "type": "deposit", "date": "2021-04-01"},
+        {"account_id": "ACC001", "amount": 4000.00, "type": "deposit", "date": "2021-04-01"},
+        {"account_id": "ACC001", "amount": 100000.00, "type": "deposit", "date": "2021-04-01"}
     ]
     assert "ACC003" not in get_transactions_by_account(sample_csv, "ACC001")
     assert len(get_transactions_by_account(sample_csv, "ACC001")) == 3
 
 def test_top_transactions(sample_csv):
     assert get_top_transaction(sample_csv) == [
-        {"account_id": "ACC004", "amount": 100000.00, "type": "withdrawal"},
-        {"account_id": "ACC001", "amount": 100000.00, "type": "deposit"}
+        {"account_id": "ACC004", "amount": 100000.00, "type": "withdrawal", "date": "2021-04-01"},
+        {"account_id": "ACC001", "amount": 100000.00, "type": "deposit", "date": "2021-04-01"}
     ]
