405caf353d4311f862af558cec1784ae5a460a3b
Lmncrt <thegord@yandex.ru>
2026-04-10 14:27:18 +0300

    Завершил написание тестов для processor.py

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -2,6 +2,7 @@ import pytest
 import csv
 from bank_analyzer import get_total_by_type
 from bank_analyzer import get_transactions_by_account
+from bank_analyzer import get_top_transaction
 
 @pytest.fixture
 def sample_csv(tmp_path):
@@ -11,7 +12,9 @@ def sample_csv(tmp_path):
         "account_id,amount,type\n"
         "ACC001,2000.00,deposit\n"
         "ACC003,3000.00,withdrawal\n"
-        "ACC001,4000.00,deposit\n",
+        "ACC001,4000.00,deposit\n"
+        "ACC004,100000.00,withdrawal\n"
+        "ACC001,100000.00,deposit\n",
         encoding="utf-8",
     )
     with open(csv_file, "r", encoding="utf-8") as f:
@@ -33,7 +36,14 @@ def test_correct_transactions_by_account(sample_csv):
     assert "ACC001" in [x['account_id'] for x in get_transactions_by_account(sample_csv, "ACC001")]
     assert get_transactions_by_account(sample_csv, "ACC001") == [
         {"account_id": "ACC001", "amount": "2000.00", "type": "deposit"},
-        {"account_id": "ACC001", "amount": "4000.00", "type": "deposit"}
+        {"account_id": "ACC001", "amount": "4000.00", "type": "deposit"},
+        {"account_id": "ACC001", "amount": "100000.00", "type": "deposit"}
     ]
     assert "ACC003" not in get_transactions_by_account(sample_csv, "ACC001")
-    assert len(get_transactions_by_account(sample_csv, "ACC001")) == 2
+    assert len(get_transactions_by_account(sample_csv, "ACC001")) == 3
+
+def test_top_transactions(sample_csv):
+    assert get_top_transaction(sample_csv) == [
+        {"account_id": "ACC004", "amount": "100000.00", "type": "withdrawal"},
+        {"account_id": "ACC001", "amount": "100000.00", "type": "deposit"}
+    ]
