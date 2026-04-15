db007c64256712e21ae4a4281bf9e7abdf61cd77
Lmncrt <thegord@yandex.ru>
2026-04-15 20:51:56 +0300

    Add functions in created class, rename processor.py to analyzer.py

---
+++ src/bank_analyzer/analyzer.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+
+class TransactionAnalyzer:
+    def __init__(self, transactions: list[dict]):
+        self.transactions: list[dict] = transactions
+
+    def get_total_by_type(self, tx_type: str) -> float:
+        result = 0.0
+        for transaction in self.transactions:
+            if transaction["type"] == tx_type:
+                result += float(transaction["amount"])
+
+        return result
+
+    def get_transactions_by_account(self, account_id: str) -> list[dict]:
+        result: list[dict] = []
+        for transaction in self.transactions:
+            if transaction["account_id"] == account_id:
+                transaction["amount"] = float(transaction["amount"])
+                result.append(transaction)
+
+        return result
+
+    def get_top_transaction(self) -> dict | None:
+        max_amount = max(float(t['amount']) for t in self.transactions)
+        return [t for t in self.transactions if float(t['amount']) == max_amount]
+
+    def generate_report(self, output_path: Path) -> None:
+        deposit = self.get_total_by_type("deposit")
+        withdrawals = self.get_total_by_type("withdraw")
+        result = {
+            "total_deposits": deposit,
+            "total_withdrawals": withdrawals,
+            "net_flow": deposit - withdrawals,
+            "top_transaction": self.get_top_transaction(),
+            "transactions_count": len(self.transactions),
+        }
+
+        # создание json файла отчета
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(result, f, indent=4, ensure_ascii=False)
